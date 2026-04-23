83572098664a4be444c17a47e260f5489e9fa6d7
Mod4 <omarelfarok24135@gmail.com>
2026-04-23 17:54:55 +0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -49,6 +49,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   export FOX_USE_LZ4_BINARY=1
   export FOX_USE_ZSTD_BINARY=1
   export FOX_USE_BUSYBOX_BINARY=1
+  export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+  export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+  export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
 
 	lunch twrp_$FDEVICE-eng
 	# let's see what are our build VARs
