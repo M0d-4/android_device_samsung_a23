9f34e62c2c7a1504972039bab7825a74e3279cb7
Mod4 <omarelfarok24135@gmail.com>
2026-04-24 00:06:22 +0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,29 +12,47 @@
 #	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #	GNU General Public License for more details.
 #
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2025 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
 # 	This software is released under GPL version 3 or any later version.
 #	See <http://www.gnu.org/licenses/>.
 #
 # 	Please maintain this if you use this script or any part of it
 #
-FDEVICE="a23"
+
 #set -o xtrace
+FDEVICE="a23"
 
 fox_get_target_device() {
-	export script_path="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-	if echo "$script_path" | grep -q "$FDEVICE"; then
-		FOX_BUILD_DEVICE="$FDEVICE"
-	elif echo "$0" | grep -q "$FDEVICE"; then
-		FOX_BUILD_DEVICE="$FDEVICE"
-	fi
+local chkdev
+  if echo "$BASH_SOURCE" | grep -q "/$FDEVICE/"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  elif set | grep BASH_ARGV | grep -w \"$FDEVICE\"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  elif echo "${BASH_SOURCE[0]}" | grep -q "/$FDEVICE/"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  elif echo "$0" | grep -q "$FDEVICE"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  fi
 }
 
 if [ -z "$FOX_BUILD_DEVICE" ]; then
 	fox_get_target_device
 fi
 
-if [ "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	echo "Detected build device: $FOX_BUILD_DEVICE"
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   # OrangeFox Addons
   export FOX_ENABLE_APP_MANAGER=1
 
@@ -56,8 +74,9 @@ if [ "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   export FOX_ENABLE_SUKISU_SUPPORT=1
   export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
   export FOX_REPLACE_TOOLBOX_GETPROP=1
-
-
 else
-	echo "I: vendorsetup.sh skipped; device mismatch or environment issue."
+	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+	fi
 fi
+
