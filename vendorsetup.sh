b1965628dd6cedc6a78c6b360bf54058ba3a49a6
Mod4 <omarelfarok24135@gmail.com>
2026-04-23 17:45:15 +0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,35 +1,62 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
 #
-# SPDX-License-Identifier: Apache-2.0
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
 #
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="a23"
+#set -o xtrace
 
-# Allow missing dependencies to prevent build halts
-export ALLOW_MISSING_DEPENDENCIES=true
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
 
-# OFRP Flags
-export OF_MAINTAINER="Mod4"
-export OF_ENABLE_FASTBOOTD=1
-export FOX_USE_TAR_BINARY=1
-export FOX_USE_LZ4_BINARY=1
-export FOX_USE_ZSTD_BINARY=1
-export FOX_USE_BUSYBOX_BINARY=1
-export OF_CLOCK_POS=1
-export OF_USE_LOCKSCREEN_BUTTON=1
-export OF_FORCE_PREBUILT_KERNEL=1
-export OF_ENABLE_LPTOOLS=1
-export OF_ENABLE_ALL_PARTITION_TOOLS=1
-export OF_STATUS_INDENT_RIGHT=80
-export OF_STATUS_INDENT_LEFT=80
-export OF_OPTIONS_LIST_NUM=8
-export OF_SCREEN_H=2400
-export OF_USE_LEGACY_BATTERY_SERVICES=1
-export OF_USE_GREEN_LED=0
-export OF_HIDE_NOTCH=1
-export OF_PATCH_AVB20=1
-export OF_FLASHLIGHT_ENABLE=0
-export OF_STATUS_H=122
-export OF_ENABLE_FRP_ADDON=1
-export OF_USE_DMCTL=1
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
 
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+
+  # OrangeFox Addons
+  export FOX_ENABLE_APP_MANAGER=1
+
+    # Binaries & Tools
+  export FOX_USE_BASH_SHELL=1
+  export FOX_ASH_IS_BASH=1
+  export FOX_USE_NANO_EDITOR=1
+  export FOX_USE_TAR_BINARY=1
+  export FOX_USE_SED_BINARY=1
+  export FOX_USE_XZ_UTILS=1
+  export FOX_USE_LZ4_BINARY=1
+  export FOX_USE_ZSTD_BINARY=1
+  export FOX_USE_BUSYBOX_BINARY=1
+
+	lunch twrp_$FDEVICE-eng
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+fi
+#
