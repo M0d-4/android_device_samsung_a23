6e2cb6d8f2378b9eb2356f056cabb96a650504f2
Mod4 <omarelfarok24135@gmail.com>
2026-04-25 13:55:14 +0400

    Delete omni_a23.mk

--- omni_a23.mk
+++
@@ -1,7 +0,0 @@
-# Inherit TWRP base
-$(call inherit-product, device/samsung/a23/twrp_a23.mk)
-
-# Inherit OrangeFox variables
-$(call inherit-product, device/samsung/a23/fox_a23.mk)
-
-PRODUCT_NAME := omni_a23
