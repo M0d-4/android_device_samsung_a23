d314b2cfe3109e7582f72cc8e1fad5f231ea05d9
Mod4 <omarelfarok24135@gmail.com>
2026-04-25 13:59:01 +0400

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -8,6 +8,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),a23)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 endif
