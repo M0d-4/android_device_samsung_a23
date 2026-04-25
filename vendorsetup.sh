dc8ca9774bf7e3335064ee8326e99af58fd4d6f7
Mod4 <omarelfarok24135@gmail.com>
2026-04-25 14:08:23 +0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -52,12 +52,11 @@ if [ -z "$FOX_BUILD_DEVICE" ]; then
 	fox_get_target_device
 fi
 
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-  # OrangeFox Addons
-  export FOX_ENABLE_APP_MANAGER=1
-
     # Binaries & Tools
+  export TW_DEFAULT_LANGUAGE="en"
+  export LC_ALL="C"
   export FOX_USE_BASH_SHELL=1
+  export FOX_ENABLE_APP_MANAGER=1
   export FOX_ASH_IS_BASH=1
   export FOX_USE_NANO_EDITOR=1
   export FOX_USE_TAR_BINARY=1
@@ -66,9 +65,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   export FOX_USE_LZ4_BINARY=1
   export FOX_USE_ZSTD_BINARY=1
   export FOX_USE_BUSYBOX_BINARY=1
+  export FOX_USE_DATE_BINARY=1
   export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
   export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-  export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
   export FOX_ENABLE_KERNELSU_SUPPORT=1
   export FOX_ENABLE_KERNELSU_NEXT_SUPPORT=1
   export FOX_ENABLE_SUKISU_SUPPORT=1
@@ -79,4 +78,5 @@ else
 		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
 	fi
 fi
+
 
