77a14339193b17ca06ba4354ff98fd7b4ef0b624
Mod4 <omarelfarok24135@gmail.com>
2026-04-29 16:36:09 +0400

    Update lunch choices for twrp_a23

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_a23.mk \
+    $(LOCAL_DIR)/twrp_a23.mk
 
 COMMON_LUNCH_CHOICES := \
-   twrp_a23-eng \
+    twrp_a23-user \
+    twrp_a23-userdebug \
+    twrp_a23-eng
